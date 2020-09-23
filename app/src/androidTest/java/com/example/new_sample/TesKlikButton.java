a28ef00ba308d5b2d9447857db50f9c652ec9199
Nurfaidah636 <70933551+Nurfaidah636@users.noreply.github.com>
2020-09-23 11:46:44 +0700

    testKlikButton

--- app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
+++ app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
@@ -26,5 +26,122 @@ public class TesKlikButton {
         onView(withId(R.id.button1)).perform(click());
         onView(withId(R.id.input)).check(matches(withText("1")));
     }
+    @Test
+    public void testKlikButtonDua(){
+        //cari button2 terus di klik
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("2")));
+    }
+    @Test
+    public void testKlikButtonTiga(){
+        //cari button3 terus di klik
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("3")));
+    }
+    @Test
+    public void testKlikButtonEmpat(){
+        //cari button4 terus di klik
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("4")));
+    }
+    @Test
+    public void testKlikButtonLima(){
+        //cari button5 terus di klik
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("5")));
+    }
+    @Test
+    public void testKlikButtonEnam(){
+        //cari button6 terus di klik
+        onView(withId(R.id.button6)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("6")));
+    }
+    @Test
+    public void testKlikButtonTujuh(){
+        //cari button7 terus di klik
+        onView(withId(R.id.button7)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("7")));
+    }
+    @Test
+    public void testKlikButtonDelapan(){
+        //cari button8 terus di klik
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("8")));
+    }
+    @Test
+    public void testKlikButtonSembilan(){
+        //cari button9 terus di klik
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("9")));
+    }
+    @Test
+    public void testKlikButtonNol(){
+        //cari button0 terus di klik
+        onView(withId(R.id.button0)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("0")));
+    }
+    @Test
+    public void testKlikButtonEqual(){
+        //cari buttonEqual terus di klik
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("=")));
+    }
+    @Test
+    public void testKlikButtonMulti(){
+        //cari buttonMulti terus di klik
+        onView(withId(R.id.button_multi)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("×")));
+    }
+    @Test
+    public void testKlikButtonDivide(){
+        //cari buttonDivide terus di klik
+        onView(withId(R.id.button_divide)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("/")));
+    }
+    @Test
+    public void testKlikButtonAdd(){
+        //cari buttonAdd terus di klik
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("+")));
+    }
+    @Test
+    public void testKlikButtonSub(){
+        //cari buttonSub terus di klik
+        onView(withId(R.id.button_sub)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("-")));
+    }
+    @Test
+    public void testKlikButtonClear(){
+        //cari buttonClear terus di klik
+        onView(withId(R.id.button_clear)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("AC")));
+    }
+    @Test
+    public void testKlikButtonPara1(){
+        //cari buttonPara1 terus di klik
+        onView(withId(R.id.button_para1)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("%")));
+    }
+    @Test
+    public void testKlikButtonPara2(){
+        //cari buttonPara2 terus di klik
+        onView(withId(R.id.button_para2)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("+/-")));
+    }
+    @Test
+    public void testKlikButtonDot(){
+        //cari buttonDot terus di klik
+        onView(withId(R.id.button_dot)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText(".")));
+    }
+
+
+
+
+
+
+
+
+
 
 }
